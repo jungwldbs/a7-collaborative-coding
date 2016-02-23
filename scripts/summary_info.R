ef191f4e4da727fd10f8caecf5909ea68ae6fdef
Ji Yoon <Jiyoon@D-69-91-172-140.dhcp4.washington.edu>
2016-02-22 20:03:32 -0800

    summary update

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -1,6 +1,7 @@
 # A function that takes in a dataset and returns a list of info about it
 summary_info <- function(data) {
   result <- list()
+  names(data) <- gsub(x = names(data), pattern = "\\.", replacement = " ")
   result$participants <- nrow(data)
   result$num_questions <- ncol(data)
   result$questions <- colnames(data)
